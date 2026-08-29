be5e6e0a3d3175bb249505bdfc7ad54f3970c4d1
david kumar <dk9025396@gmail.com>
2026-08-30 01:02:30 +0530

    first code added

---
+++ environments/prod/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "azurerm" {
+    
+  }
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.80.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
